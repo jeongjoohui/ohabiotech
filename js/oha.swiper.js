81e2d35dc6b663093bf8139a635e7017bff3c96d
kmmc <wngnl823@koreammc.com>
2021-01-12 17:50:13 +0900

    new arrival 영역 작업진행중, swiper 미연결

---
+++ js/oha.swiper.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+});
