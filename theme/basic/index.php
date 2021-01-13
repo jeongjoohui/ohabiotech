20bc5fed34eb8a9f84127fad456e1e44cf85afa8
kmmc <wngnl823@koreammc.com>
2021-01-13 13:56:44 +0900

    section4까지 작업 완료

--- theme/basic/index.php
+++ theme/basic/index.php
@@ -15,7 +15,7 @@ include_once(G5_THEME_PATH.'/head.php');
 
 ?>
 
-
+<!-- section1 -->
 <section id="main_banner">
     <div id="main_video">
 
@@ -28,8 +28,8 @@ include_once(G5_THEME_PATH.'/head.php');
         <!-- <button class="main_btn">자세히보기 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;></button> -->
     </div>
 </section>
-
-<section id="section2">
+<!-- section2 -->
+<section id="section2" class="clearfix">
     <span class="ver_line"></span>
     <h2 class="title"><a href="<?php echo get_pretty_url('content', 'company');  ?>">OHA BIOTECH STORY</a></h2>
     <h3 class="sub_title">오하바이오텍을 소개합니다.</h3>
@@ -57,25 +57,51 @@ include_once(G5_THEME_PATH.'/head.php');
         </li>
     </ul>
 </section>
-
-<section id="section3">
+<!-- section3 -->
+<section id="section3" class="clearfix">
     <h2 class="title"><a href="<?php echo get_pretty_url($bo_table); ?>">NEW AVRRIVAL</a></h2>
     <h3 class="sub_title">오하바이오텍에서 새롭게 출시된 신상품을 소개합니다.
     </h3>
     <!-- swiper -->
     <div class="swiper-container">
         <div class="swiper-wrapper">
-            <div class="swiper-slide"><img src="" alt=""></div>
-            <div class="swiper-slide">Slide 2</div>
-            <div class="swiper-slide">Slide 3</div>
-            <div class="swiper-slide">Slide 4</div>
-            <div class="swiper-slide">Slide 5</div>
+            <div class="swiper-slide"><a href=""><img src="<?php echo G5_IMG_URL ?>/3d_w.jpg" alt=""></a></div>
+            <div class="swiper-slide"><a href=""><img src="<?php echo G5_IMG_URL ?>/3d_b.jpg" alt=""></a></div>
+
+            <div class="swiper-slide"><a href=""><img src="<?php echo G5_IMG_URL ?>/2d_w.jpg" alt=""></a></div>
+
+            <div class="swiper-slide"><a href=""><img src="<?php echo G5_IMG_URL ?>/2d_b.jpg" alt=""></a></div>
+
+            <div class="swiper-slide"><a href=""><img src="<?php echo G5_IMG_URL ?>/den_w.jpg" alt=""></a></div>
+
+            <div class="swiper-slide"><a href=""><img src="<?php echo G5_IMG_URL ?>/den_b.jpg" alt=""></a></div>
+
+            <div class="swiper-slide"><a href=""><img src="<?php echo G5_IMG_URL ?>/2dc_w.jpg" alt=""></a></div>
+            <div class="swiper-slide"><a href=""><img src="<?php echo G5_IMG_URL ?>/2dc_b.jpg" alt=""></a></div>
+
+
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
         <!-- Add Arrows -->
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
+    </div>
+</section>
+<!-- section4 -->
+<section id="section4" class="clearfix">
+    <img src="<?php echo G5_IMG_URL ?>/section4_banner.jpg" alt="">
+</section>
+<!-- section5 -->
+<section id="section5" class="clearfix">
+    <h2 class="title"><a href="<?php echo get_pretty_url('content', 'company');  ?>">CONTACT US</a></h2>
+    <h3 class="sub_title">궁금한 사항이나 기타 문의사항이 있으신 경우 언제든 연락 부탁드립니다.</h3>
+    <div class="contact_us">
+        <div class="cont_lf">
+            <h2>CONTACT US</h2>
+        </div>
+
+
     </div>
 </section>
 
